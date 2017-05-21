375e066deea0ae5e266c2b3f783ffcd42ff026e9
Hector Riquelme <hector.riquelme1169@gmail.com>
2017-05-21 15:28:25 -0400

    JWT Middleware

---
+++ README.md
@@ -0,0 +1,1 @@
+# express-kit-starter
--- app/Router.js
+++ app/Router.js
@@ -1,11 +1,13 @@
 import express from 'express'
+import jwt from 'express-jwt'
+import { config } from '../config/config'
 import { SampleRoutes } from './routes/SampleRoute'
 import { SampleMiddleware } from './middlewares/SampleMiddleware'
 import { JWTRoutes } from './routes/JWTRoute'
 
 export const Router = [{
     path: "/",
-    middleware: [SampleMiddleware.anyCheck, SampleMiddleware.anyCheckTwo],
+    middleware: [jwt({secret: config.SECRET}), SampleMiddleware.anyCheck, SampleMiddleware.anyCheckTwo],
     handler: SampleRoutes,
 },{
     path: "/JWT",
